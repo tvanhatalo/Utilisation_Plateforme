335867a798146f54738e752504ed29a1a55aab8b
tvanhatalo <tara.vanhatalo@gmail.com>
2018-09-18 13:21:08 +0200

    Aucune modification dans le code, juste le pas h

--- tp1/src/programme.py
+++ tp1/src/programme.py
@@ -10,10 +10,10 @@ import equations
 # Résolution approchée de y' = 2-y avec y(0) = 3 pour t dans [0,5]
 equations.a = -1.
 equations.b = 1. #originally 2
-t0,y0 = 0.,0. #3
+t0,y0 = 0.,5. #3
 T = 1. #5
 # Avec un pas de 0.1, il faut donc 50 iterations
-N,h = 50,0.1
+N,h = 50,0.00625
 [t,y1] = methodes.euler_explicite(t0,h,N,y0,equations.f_affine)
 #[t,y1] = methodes.euler_explicite(t0,h,N,y0,equations.f_carree)
 # Solution exacte aux mêmes instants
@@ -22,15 +22,15 @@ z1 = equations.sol_affine(t,y0)
 # Calcul de l'erreur maximum relative
 e1 = np.max(np.abs((z1-y1)/z1))
 # Graphe des solutions exactes et approchées
-plt.loglog(t,y1,'b-+',label='solution approchée h=0.1')
+plt.loglog(t,y1,'b-+',label='solution approchée h=0.00625')
 plt.loglog(t,z1,'r-',label='solution exacte')
 plt.xlabel('t')
 plt.ylabel('y')
 plt.title("Méthode d'Euler explicite")
 #plt.title("Méthode de RK2")
 plt.legend()
-#plt.savefig("Pas02.png")
-plt.show("Pas02.png")  #il le faut en echelle log
+plt.savefig("Pas000625.png")
+plt.show("Pas000625.png")  #il le faut en echelle log
 
 # Écriture de l'erreur en fonction de h
 print("{0} | {1}".format(h,e1))
