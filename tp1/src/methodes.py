3f07941fb1da856c5963b66c37df61d4423b568c
tvanhatalo <tara.vanhatalo@gmail.com>
2018-09-18 13:08:03 +0200

    Ajout de la méthode RK2

--- tp1/src/methodes.py
+++ tp1/src/methodes.py
@@ -27,3 +27,28 @@ def euler_explicite(t0,h,N, y0,f):
 
     return [t,y]
 
+
+
+def RK2(t0,h,N,y0,f):
+    """Méthode de Runge-Kutta 2 pour la résolution d'un problème de Cauchy.
+
+    Resolution de y'(t) = f(t,y(t)) avec y(t0) = y0 par la méthode de Runge-
+    Kutta 2 avec un pas de temps h>0 et pour t = k*h, k=0..N.
+
+    """
+
+    # Construit un tableau des temps: t = t0 + [0,h,2h,...ih,...Nh]
+    t = np.linspace(t0,t0+N*h,N+1) # N+1 temps de t0 à t0+N*h
+
+    # Alloue un tableau de la taille de t, initialisé à 0. Il servira à
+    # stocker la solution aux instants t0+i*h.
+    y = 0.*t
+
+    # Donnée initiale, la valeur [0] du tableau y
+    y[0] = y0
+    
+    # On doit faire N itérations en temps
+    for k in np.arange(N): # Boucle de 0 à N-1
+        y[k+1] = y[k] + h*f(t[k]+0.5*h,y[k]+0.5*f(t[k],y[k])) # Formule de la méthode de Runge-Kutta 2
+
+    return [t,y]
